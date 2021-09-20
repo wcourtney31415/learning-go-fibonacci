bfbf4a59f95ca99fede5962266923d955769454b
wcourtney31415 <wcourtney31415@gmail.com>
2021-09-19 23:50:47 -0400

    Renamed variables, and reduced line count by performing initialization on the declaration line of the slice.

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	s := make([]int, 2)
-	s[0] = 0
-	s[1] = 1
+	list := []int{0, 1}
 	for i := 0; i < 20; i++ {
-		var a = len(s) - 1
-		var b = len(s) - 2
-		var sum = a + b
-		s = append(s, sum)
+		var listLength = len(list)
+		var last = listLength - 1
+		var secondToLast = listLength - 2
+		var sum = last + secondToLast
+		list = append(list, sum)
 	}
-	fmt.Println(s)
+	fmt.Println(list)
 }
